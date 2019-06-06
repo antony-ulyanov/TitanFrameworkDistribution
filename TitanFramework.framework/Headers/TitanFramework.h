6babae0fc68b0f315b0408d4cb5dab6c0475e6cd
antony <AUlyanov@medlinesoft.ru>
2019-06-06 16:04:05 +0300

    Added TitanFramework

---
+++ TitanFramework.framework/Headers/TitanFramework.h
@@ -0,0 +1,19 @@
+//
+//  TitanFramework.h
+//  TitanFramework
+//
+//  Created by ANTON ULYANOV on 22.06.2018.
+//  Copyright © 2018 medlinesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TitanFramework.
+FOUNDATION_EXPORT double TitanFrameworkVersionNumber;
+
+//! Project version string for TitanFramework.
+FOUNDATION_EXPORT const unsigned char TitanFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TitanFramework/PublicHeader.h>
+//#import <TitanFramework/AVAudioSession+Swift.h>
+
